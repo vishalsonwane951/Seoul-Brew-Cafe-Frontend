b1d49eeeda3544255c55d74b1c30d157bf4e9936
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-09-09 09:39:24 +0530

    fix:backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,47 +1,6 @@
-// import axios from "axios";
-
-// const API = axios.create({        
-//   baseURL: "http://localhost:5000/api",
-//   // baseURL: "https://seoul-brew-cafe-backend.onrender.com/api",
-//   // baseURL: "https://seoul-brew-cafe-backend-1.onrender.com/api",
-//   // baseURL: "http://3.109.183.192:5000/api",
-//   // baseURL: "https://ec2-3-109-183-192.ap-south-1.compute.amazonaws.com:5000/api",
-//   //  3.109.183.192    
-
-// });
-
-// API.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) config.headers.Authorization = `Bearer ${token}`;
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// // Derives upload base from the same env var — no duplication
-// export const UPLOAD_BASE = (import.meta.env.VITE_API_URL || "http://locahost:500/api")
-//   .replace("/api", "");
-
-// API.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     if (err.response?.status === 401) {
-//       console.warn("Session expired or unauthorized:", err.response?.data);
-//     }
-//     return Promise.reject(err);
-//   }
-// );
-
-// export default API;
-
-
-
-
 import axios from "axios";
 
 const VITE_API_URL = 'https://seoul-brew-cafe-backend-1.onrender.com/api'
-// const VITE_API_URL = 'https://ec2-3-109-183-192.ap-south-1.compute.amazonaws.com:5000/api'
 // const VITE_API_URL = 'http://localhost:5000/api'
 
 const API = axios.create({        
@@ -57,7 +16,7 @@ API.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-export const UPLOAD_BASE = (VITE_API_URL || "http://localhost:5000/api")
+export const UPLOAD_BASE = (VITE_API_URL)
   .replace("/api", "");
 
 API.interceptors.response.use(
